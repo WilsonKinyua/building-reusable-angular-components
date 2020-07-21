40592a311d9ecad1ad363b542f039e092f5fd414
WilsonKinyua <wilsonkinyuam@gmail.com>
2020-07-21 18:58:00 +0300

    end of creating reusable components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { FavoriteChangedEventArgs } from './favorite/favorite.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  post = {
+    title: 'Title',
+    isFavorite: true
+  };
+  //  onFavoriteChanged(eventArgs: { FavoriteChangedEventArgs }) {
+  //   console.log('Favorite changed', eventArgs);
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoriteComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule
